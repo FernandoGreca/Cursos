cd972daa76981bd6acc13b3687f76f6d002577be
Fernando Greca <144340988+FernandoGreca@users.noreply.github.com>
2024-08-07 14:53:24 -0300

    .

--- logica-de-programacao/secao-12/soma_vetor/obj/Debug/net8.0/soma_vetor.AssemblyInfo.cs
+++ logica-de-programacao/secao-12/soma_vetor/obj/Debug/net8.0/soma_vetor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("soma_vetor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e27f880e7528eb53f7f274df0c310edd341b83c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f179b71de2910e3576cd46561b24efee26ed676")]
 [assembly: System.Reflection.AssemblyProductAttribute("soma_vetor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("soma_vetor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
